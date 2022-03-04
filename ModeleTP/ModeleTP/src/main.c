7e042f6db5bfcaf28c3bddc28352eaf5328db438
Nunez Dorian <doriannunez64@gmail.com>
2022-03-04 10:48:36 +0100

    Le bras bouge horizontalement de 10°

--- ModeleTP/ModeleTP/src/main.c
+++ ModeleTP/ModeleTP/src/main.c
@@ -1,11 +1,29 @@
 #include "libTPIUT.h"
 #include "robot.h"
 
-int main (void)
-{
+void init(){
 	initCarte();
 	configureConsole();
-	while(true) {
+	initRobot();
+	//On démarre le bouton BP1
+	allumerPeriph(PortB);
+	programmerLigne(PortB,25, ENTREE);
+	activerFiltrageLigne(PortB,25,30);
+	
+}
 
+int main (void)
+{
+	init();
+	int tabPosition[] = {10,0,-10,0};
+	int i = 0;
+	while(true) {
+		if(lireLigne(PortB,25)==1)
+		{
+			positionnerMoteur(BASE,tabPosition[i]);
+			i=(i+1)%4;
+			
+			while (lireLigne(PortB,25));
+		}
 	}
 }
