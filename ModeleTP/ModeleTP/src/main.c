d8c1502ff5f817e6e4ee129ccbaae96b2e440300
Nunez Dorian <doriannunez64@gmail.com>
2022-03-04 11:27:14 +0100

    La del gauche s'allume pour la base,celle de droite pour l'epaule

--- ModeleTP/ModeleTP/src/main.c
+++ ModeleTP/ModeleTP/src/main.c
@@ -13,6 +13,10 @@ void init(){
 	allumerPeriph(PortC);
 	programmerLigne(PortC,26,ENTREE);
 	activerFiltrageLigne(PortC,26,30);
+	//On demarre les dels
+	
+	programmerLigne(PortC,30,SORTIE0);//Gauche
+	programmerLigne(PortB,27,SORTIE0);//Droite
 }
 
 int main (void)
@@ -23,14 +27,26 @@ int main (void)
 	int indiceMoteur = 0;
 	int i = 0;
 	while(true) {
+		if(indiceMoteur)
+		{
+			programmerLigne(PortB,27,SORTIE1);
+			programmerLigne(PortC,30,SORTIE1);
+		}
+		else
+		{
+			programmerLigne(PortB,27,SORTIE0);
+			programmerLigne(PortC,30,SORTIE0);
+		}
 		if(lireLigne(PortB,25))
 		{
 			positionnerMoteur(indiceMoteur,tabAngle[i]);
 			i=(i+1)%4;
+			
 			while (lireLigne(PortB,25));
 		}
 		if(lireLigne(PortC,26))
 		{
+			
 			indiceMoteur=(indiceMoteur+1)%2;
 			while(lireLigne(PortC,26));
 		}
