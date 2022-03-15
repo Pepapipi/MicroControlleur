6491fd2c4ba9b422169b15f0692500471ca2c198
Nunez Dorian <doriannunez64@gmail.com>
2022-03-15 10:22:22 +0100

    Tp2 ex2

--- ModeleTP/ModeleTP/src/main.c
+++ ModeleTP/ModeleTP/src/main.c
@@ -22,27 +22,17 @@ void init(){
 	//Timer
 	allumerPeriph(Timer1);
 	timerModeDelai(Timer1, HDIV2, 1680000UL, REPETITIF, INC);
-}
-
-int tabAngle[] = {30,0,-30,0};
-//BASE = 0 , EPAULE = 1
-int indiceMoteur = 0;
-int tabMoteur[] = {0,0,0,0,0};
-void retourAZero()
-{
-	for (int i=0;i<=4;i++)
-	{
-		ecrireLigne(PortB,27,1);
-		ecrireLigne(PortA,21,0);
-		ecrireLigne(PortC,30,0);
-		positionnerMoteur(i,0);
-		tabMoteur[i]=0;
-	}
+	//Timer
+	allumerPeriph(Timer2);
+	timerModeDelai(Timer2, HDIV2, 42000000U, NON_REPETITIF, INC);
+	allumerPeriph(Timer0);
+	timerModeMesure(Timer0,HDIV2,LIMITE_TIMER_MAX, DE_1_A_0);
+	programmerLigne(PortB,25,FONCTIONB);
 }
 void positionnerMoteurLent(int moteur,int angleActuel, int angleSuivant)
 {
 	lancerTimer(Timer1);
-	printf("L'angle actuel : %d, l'angle suivant: %d \n", angleActuel, angleSuivant);
+	//printf("L'angle actuel : %d, l'angle suivant: %d \n", angleActuel, angleSuivant);
 	
 	if (angleActuel>=angleSuivant)
 	{
@@ -62,6 +52,25 @@ void positionnerMoteurLent(int moteur,int angleActuel, int angleSuivant)
 	}
 	arreterTimer(Timer1);
 }
+
+int tabAngle[] = {0,30,0,-30};
+//BASE = 0 , EPAULE = 1
+int indiceMoteur = 0;
+int tabMoteur[] = {0,0,0,0,0};
+void retourAZero()
+{
+	lancerTimer(Timer2);
+	for (int i=0;i<=4;i++)
+	{
+		ecrireLigne(PortB,27,1);
+		ecrireLigne(PortA,21,0);
+		ecrireLigne(PortC,30,0);
+		positionnerMoteurLent(i,tabAngle[tabMoteur[i]],0);
+		tabMoteur[i]=0;
+	}
+	arreterTimer(Timer2);
+}
+
 int main (void)
 {
 	init();
@@ -79,23 +88,27 @@ int main (void)
 			ecrireLigne(PortA,21,0);
 			ecrireLigne(PortC,30,0);
 		}
-
-		if(lireLigne(PortB,25))
+		lancerTimer(Timer0);
+	
+		if(lireLigne(PortB,25)&&testerEtatTimer(Timer0,MESURE))
 		{
-			if(indiceMoteur!=5)
+			printf("Duree de pression %d \n",lireMesureTimer(Timer0));
+			if(lireMesureTimer(Timer0)<200000000)
 			{
+				printf("Mouvement moteur \n");
 				positionnerMoteurLent(indiceMoteur,tabAngle[tabMoteur[indiceMoteur]],tabAngle[(tabMoteur[indiceMoteur]+1)%4]);
 				tabMoteur[indiceMoteur]=(tabMoteur[indiceMoteur]+1)%4;
 			}
 			else
 			{
+				printf("Mouvement zero \n");
 				retourAZero();
 			}
 			while (lireLigne(PortB,25));
+	
 		}
 		if(lireLigne(PortC,26))
-		{
-	
+		{ 
 			indiceMoteur=(indiceMoteur+1)%6;
 			while(lireLigne(PortC,26));
 		}
